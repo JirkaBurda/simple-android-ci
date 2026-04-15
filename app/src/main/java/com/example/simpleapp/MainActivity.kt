39a2f895131847efc55e667251c93b8af9d5815f
JirkaBurda <BurdaTD@seznam.cz>
2026-04-15 14:45:07 +0200

    Create MainActivity.kt

---
+++ app/src/main/java/com/example/simpleapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.simpleapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        val text = findViewById<TextView>(R.id.text)
+
+        button.setOnClickListener {
+            text.text = "OK"
+        }
+    }
+}
